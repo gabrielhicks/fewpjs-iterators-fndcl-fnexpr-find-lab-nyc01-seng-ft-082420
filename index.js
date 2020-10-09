c3722b5e2652a888c63946b3f1fb90c1632ea31c
gabrielhicks <gabrielhicks@mail.com>
2020-10-09 13:28:32 -0400

    done

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find(when => when.result === "W")
+  if (win === undefined) {
+    return undefined
+  } else {
+    return win.year
+  }
+}
